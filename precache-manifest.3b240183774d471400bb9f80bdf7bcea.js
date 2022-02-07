762d943ffe0819ded7592018e96bad0c61d2e8da
minbin <tammylam0@gmail.com>
2022-02-06 18:15:35 -0600

    Updates

---
+++ precache-manifest.3b240183774d471400bb9f80bdf7bcea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d51e4554e242923e2a35e8337d03ad21",
+    "url": "/cosc-4353/index.html"
+  },
+  {
+    "revision": "b19c9a991270b790c77d",
+    "url": "/cosc-4353/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c3fc076a3f51c1bab3c1",
+    "url": "/cosc-4353/static/js/2.a2c4eadd.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/cosc-4353/static/js/2.a2c4eadd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "936196030f257a07f32e",
+    "url": "/cosc-4353/static/js/3.7e47dc25.chunk.js"
+  },
+  {
+    "revision": "b19c9a991270b790c77d",
+    "url": "/cosc-4353/static/js/main.4938b899.chunk.js"
+  },
+  {
+    "revision": "37f5dd2cd11304c34e88",
+    "url": "/cosc-4353/static/js/runtime-main.707438b7.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/cosc-4353/static/media/logo.06e73328.svg"
+  }
+]);
